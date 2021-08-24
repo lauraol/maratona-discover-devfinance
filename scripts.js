048b5c33c4d10ef93c3a6f77abf400585ec9cf57
lauraol <lauraoliveirape@gmail.com>
2021-08-24 15:40:02 -0300

    correção na função de formatar o dinheiro

--- scripts.js
+++ scripts.js
@@ -116,11 +116,9 @@ const DOM = {
 
 const Utils = {
   formatAmount(value) {
-    value = Number(value.replace(/\,\./g, "")) * 100;
-    //value = Number(value) * 100;
+    value = value * 100;
 
-    return value;
-    //console.log(value);
+    return Math.round(value);
   },
 
   formatDate(date) {
